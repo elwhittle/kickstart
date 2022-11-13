802369220d3a8ab659052fc4e40ad505055fc93e
elijah <elijah.d.whittle@gmail.com>
2022-11-13 12:38:16 -0500

    after participation

--- 2022/H/1.cc
+++ 2022/H/1.cc
@@ -1,20 +1,38 @@
 #include <iostream>
+// #include <algorithm>
 // #include <vector>
 // #include <string>
 
 using namespace std;
 
-int solution(int n, int m) { 
-
-  return 0; 
+long long solution(long long l, int n) { 
+  char dir = 'C';
+  long long x, dist = 0, laps = 0;
+  char newDir;
+  for (int i = 0; i < n; ++i) {
+    cin >> x >> newDir;
+    if (newDir == dir) {
+      dist += x;
+    } else {
+      dist -= x;
+      if (dist <= 0) {
+        dir = newDir;
+        dist = -dist;
+      }
+    }
+    laps += dist / l;
+    dist %= l;
+  }
+  return laps; 
 }
 
 int main() {
-  int t, n, m;
+  int t, n;
+  long long l;
   cin >> t; // read t. cin knows that t is an int, so it reads it as such.
   for (int i = 1; i <= t; ++i) {
-    cin >> n >> m;
-    cout << "Case #" << i << ": " << solution(n, m) << " " << (n * m) << endl;
+    cin >> l >> n;
+    cout << "Case #" << i << ": " << solution(l, n) << endl;
   }
   return 0;
 }
